27557b365e6a2eb619d83ebe552edd1ca63ba655
RamyaSathyamoorthy <ramyaasathyamoorthy@gmail.com>
2022-09-07 01:52:09 +0530

    sukesh code

--- src/test/java/org/test1/ProjectClass2.java
+++ src/test/java/org/test1/ProjectClass2.java
@@ -1,5 +1,11 @@
 package org.test1;
 
 public class ProjectClass2 {
+	 public static void main(String[] args) {
+		 System.out.println("Test1");
+		 System.out.println("Test4");
+		 System.out.println("Test5");
+		 
 
+}
 }
